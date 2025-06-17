96fe7aa8c3af55bbbf83c588d0d70df3de39c45c
LENOVO <surajsh4646@gmail.com>
2025-06-17 23:09:05 +0530

    Euclidean distance between two points as well as the equation of the line using those two points

--- Methods/NumberCheckerNew.java
+++ Methods/NumberCheckerNew.java
@@ -3,7 +3,7 @@ package Methods;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class NumberCheckerThree {
+public class NumberCheckerNew {
     public static int countDigits(int num) {
         return String.valueOf(num).length();
     }
